455b250f6a8a6e9e40e051d55bdc50d2886a0489
vishalsha <vishuf4@gmail.com>
2021-06-07 20:49:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1></h1>'
+	return '<h1>Flask App</h1>'
 
 @app.route('/<name>')
 def user(name):
 	return '<h1>{}</h1>'.format(name)
 
+@app.route('topRated/<int: topNIds>')
+def getTopRated(topNIds):
+	return 'List of top {} Ids are: {}'.format(topNIds,[1111,2222,3333])
 
 if __name__ == "__main__":
 	app.run(host='localhost', port=5000)
